8bf927af2843cafe99ada637012d6ac321df40e0
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2021-04-25 23:39:57 +0530

    Refactored the whole code base

--- app/src/main/java/com/example/workoutadvisor/MainActivity.java
+++ app/src/main/java/com/example/workoutadvisor/MainActivity.java
@@ -1,35 +1,68 @@
 package com.example.workoutadvisor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener{
+
+    private ListView exerciseListView;
+    private Button checkBtn;
+    private Spinner spinner;
+
+    public String TAG = "mainActivity.BodyPart";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Init();
+        setUpTheSpinner();
+        spinner.setOnItemSelectedListener(this);
+
+        checkBtn.setOnClickListener(view -> {
+            Toast.makeText(this, spinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+        });
     }
 
-    public void check(View view) {
-        Workoutlist w = new Workoutlist();
-        TextView workouts=(TextView)findViewById(R.id.textView);
-        Spinner spinner=(Spinner)findViewById(R.id.spinner);
-        String type=String.valueOf(spinner.getSelectedItem());
-        List<String> list = w.getworkouts(type);
-        StringBuilder s= new StringBuilder();
-        for(String work : list){
-            s.append(work).append('\n').append("...").append("\n");
-        }
-        s.append("Done for today");
-        workouts.setText(s);
+    private void setUpTheSpinner() {
+        // Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.workout_types));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
 
+    private void Init() {
+        exerciseListView = findViewById(R.id.exercise_list_view);
+        checkBtn = findViewById(R.id.check_exercise_btn);
+        spinner = findViewById(R.id.spinner);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String bodyPart = spinner.getSelectedItem().toString();
+        Log.i(TAG,bodyPart);
+
+        Workoutlist workoutList = new Workoutlist();
+        ArrayAdapter<String> workoutListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,workoutList.getworkouts(bodyPart));
+        exerciseListView.setAdapter(workoutListAdapter);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        Toast.makeText(this, "Nothing is selected till now.", Toast.LENGTH_SHORT).show();
     }
 }
